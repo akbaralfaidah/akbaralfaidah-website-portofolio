332a8b8892981710969c2a83d14466c0138ca628
akbaralfaidah <akbar.al.faidah0120@gmail.com>
2026-08-21 14:08:47 +0700

    feat: add sitemap plugin and structured schema metadata for improved SEO

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://akbaralfaidah.com',
+      dynamicRoutes: [
+        '/projects',
+        '/project/bosdepot',
+        '/project/siabsen',
+        '/project/peka',
+        '/project/chattask'
+      ],
+      robots: [{ userAgent: '*', allow: '/' }]
+    })
+  ],
   server: {
     headers: {
       'X-Frame-Options': 'DENY',
